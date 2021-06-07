0312ec54b801a28530578b4fe98fb29775f23b4e
Arie Trouw <arie.trouw@xy.company>
2021-06-07 12:09:49 -0700

    exporting fields types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
+import FunnelStartedFields from './FunnelStartedFields'
 import XyPixel from './Pixel'
 import Referrer from './Referrer'
+import TestStartedFields from './TestStartedFields'
 import UniqueUserId from './UniqueUserId'
+import UserClickFields from './UserClickFields'
 import UserEventHandler from './UserEventHandler'
 import UtmFields from './UtmFields'
+import ViewContentFields from './ViewContentFields'
 import XyUserEventHandler from './XyUserEventHandler'
 
 export * from './Api'
 
 export { Referrer, UniqueUserId, UserEventHandler, UtmFields, XyPixel, XyUserEventHandler }
+export type { FunnelStartedFields, TestStartedFields, UserClickFields, ViewContentFields }
