e51cfbd136eeaad50b7ff40b7d560f7614ea64ce
Arie Trouw <arie.trouw@xy.company>
2021-07-14 16:05:02 -0700

    switched to bowser

--- src/Api/UserEventSystem.ts
+++ src/Api/UserEventSystem.ts
@@ -1,20 +1,5 @@
-interface UserEventSystem {
-  userAgent?: string
-  is?: {
-    bot?: boolean
-    mobileBot?: boolean
-    desktopMode?: boolean
-    tv?: boolean
-    webKit?: boolean
-    console?: boolean
-    watch?: boolean
-  }
-  mobile?: string
-  phone?: string
-  tablet?: string
-  os?: string
-  userAgents?: string[]
-  versions?: Record<string, string>
-}
+import Bowser from 'bowser'
+
+type UserEventSystem = Bowser.Parser.ParsedResult
 
 export default UserEventSystem
