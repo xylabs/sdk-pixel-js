e1ad5dd7135c445393dfdb55d67a199f235238be
Arie Trouw <arie.trouw@xy.compay>
2021-08-02 11:24:00 -0700

    api config with preset or specific uri

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -2,11 +2,18 @@ import axios from 'axios'
 
 import UserEvent from './UserEvent'
 
+const apiBaseUri: Record<string, string> = {
+  beta: 'https://beta.api.coinapp.co',
+  local: 'http://localhost:3030/dev',
+  prod: 'https://api.coinapp.co',
+}
+
 class PixelApi {
   private endPoint: string
 
-  constructor(baseUri = 'https://api.coinapp.co') {
-    this.endPoint = `${baseUri}/t/event`
+  /* baseUri can either be a preset (prod, beta, local), or a specific uri */
+  constructor(baseUri = 'prod') {
+    this.endPoint = apiBaseUri[baseUri] ? `${apiBaseUri[baseUri]}/t/event` : baseUri
   }
 
   public async trackEvents(events: UserEvent[]) {
