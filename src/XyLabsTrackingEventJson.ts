1a68f36e432417aadf0cd03ac42316843ea05c39
Arie Trouw <arie.trouw@xylabs.com>
2021-12-21 16:17:44 -0800

    pixel model

---
+++ src/XyLabsTrackingEventJson.ts
@@ -0,0 +1,17 @@
+export interface XyLabsTrackingEventJson {
+  cid: string
+  create_time?: number
+  email?: string
+  email_hash?: string
+  event?: string
+  exids?: Record<string, string>
+  fields?: Record<string, unknown>
+  host?: string
+  pathname?: string
+  pixel?: string
+  receive_time?: number
+  rid?: string
+  uid?: string
+  utm?: Record<string, string>[] | Record<string, string[]>
+  system?: unknown
+}
