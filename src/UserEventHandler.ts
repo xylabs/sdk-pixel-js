ddd8f03ea78a0f89f9fb3e67b3bd320f1485790a
Arie Trouw <arie.trouw@xylabs.com>
2024-09-06 12:22:05 -0700

    UserEventHandler types - 2.0.4

--- src/UserEventHandler.ts
+++ src/UserEventHandler.ts
@@ -1,11 +1,13 @@
+import type { EmptyObject } from '@xylabs/object'
+
 import type { FunnelStartedFields } from './FunnelStartedFields.js'
 import type { TestStartedFields } from './TestStartedFields.js'
 import type { UserClickFields } from './UserClickFields.js'
 import type { ViewContentFields } from './ViewContentFields.js'
 
-export abstract class UserEventHandler<T> {
-  abstract funnelStarted(fields: T | FunnelStartedFields): Promise<void>
-  abstract testStarted(fields: T | TestStartedFields): Promise<void>
-  abstract userClick(fields: T | UserClickFields): Promise<void>
-  abstract viewContent(fields: T | ViewContentFields): Promise<void>
+export abstract class UserEventHandler<TData extends EmptyObject> {
+  abstract funnelStarted<T extends TData>(fields: T | FunnelStartedFields): Promise<void>
+  abstract testStarted<T extends TData>(fields: T | TestStartedFields): Promise<void>
+  abstract userClick<T extends TData>(fields: T | UserClickFields): Promise<void>
+  abstract viewContent<T extends TData>(fields: T | ViewContentFields): Promise<void>
 }
