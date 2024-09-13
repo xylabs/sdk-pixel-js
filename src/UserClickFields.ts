ff2b1320ba3a6f82d21660766eb558346e58cb41
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-09-13 16:54:42 -0400

    lint

--- src/UserClickFields.ts
+++ src/UserClickFields.ts
@@ -3,6 +3,6 @@ import type { CommonFields } from './CommonFields.js'
 export interface UserClickFields extends CommonFields {
   elementName: string
   elementType: string
-  placement?: string
   intent?: string
+  placement?: string
 }
